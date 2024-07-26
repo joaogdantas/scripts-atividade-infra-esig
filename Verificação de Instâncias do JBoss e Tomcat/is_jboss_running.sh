0122d243a7dd6ef9d336983cc81e39543d83c222
João Dantas <joaogdantasdev@hotmail.com>
2024-07-26 20:55:45 -0300

    Adicionado tempo de execução das instancias

--- Verificação de Instâncias do JBoss e Tomcat/is_jboss_running.sh
+++ Verificação de Instâncias do JBoss e Tomcat/is_jboss_running.sh
@@ -2,6 +2,8 @@
 
 if ps aux | grep -q "[j]boss"; then
     echo "JBoss está rodando."
+
+    ps -eo pid,etime,cmd | grep "[j]boss"
 else
     echo "JBoss não está rodando."
 fi
--- Verificação de Instâncias do JBoss e Tomcat/is_tomcat_running.sh
+++ Verificação de Instâncias do JBoss e Tomcat/is_tomcat_running.sh
@@ -2,6 +2,8 @@
 
 if ps aux | grep -q "[c]atalina"; then
     echo "Tomcat está rodando."
+    
+    ps -eo pid,etime,cmd | grep "[c]atalina"
 else
     echo "Tomcat não está rodando."
 fi
